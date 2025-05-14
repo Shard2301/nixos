8613ee253449774ec0463c66c3eee3d16492a4c7
Commodore3193 <lindleyjohn@protonmail.ch>
2025-05-14 17:06:44 +1200

    initial: initial home-manager configuration state

--- flake.nix
+++ flake.nix
@@ -2,21 +2,43 @@
   description = "NixOS Version Control Flake";
 
   inputs = {
+    ### Flakes
     # Nix Package Repositories
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    nixpkgs-old.url = "github:nixos/nixpkgs?ref=nixos-24.11";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable"; # Current Stable
+    nixpkgs-old.url = "github:nixos/nixpkgs?ref=nixos-24.11"; # Unstable
+
+    # Home manager
+    home-manager = {
+      url = "github:nix-community/home-manager?ref=master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, nixpkgs-old, ... }@inputs:
-  let
+  outputs = {
+    self,
+    nixpkgs,
+    nixpkgs-old,
+    home-manager,
+    ...
+  } @ inputs: let
     system = "x86_64-linux";
     pkgs = "nixpkgs.legacyPackages.${system}";
     pkgs-old = "nixpkgs-old.legacyPackages.${system}";
   in {
     nixosConfigurations.nixos-desktop = nixpkgs.lib.nixosSystem {
-      specialArgs = { inherit inputs system; };
-      modules = [ 
-        ./configuration.nix 
+      specialArgs = {inherit inputs system;};
+      modules = [
+        ./configuration.nix
+
+        # Home Manager Module
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.john = import ./home.nix;
+          home-manager.extraSpecialArgs = {inherit inputs;};
+          home-manager.backupFileExtension = "bak";
+        }
       ];
     };
   };
---
+++ home.nix
@@ -0,0 +1,34 @@
+{
+  inputs,
+  config,
+  lib,
+  pkgs,
+  home-manager,
+  ...
+}: {
+  # Username and Home Directory that home-manager should manager
+  home.username = "john";
+  home.homeDirectory = "/home/john";
+
+  # Allow home-manager to install and manage itself
+  programs.home-manager.enable = true;
+
+  # This variable declares the version of home-manager upon initial install
+  # This ensures compatibility in case of breaking changes introduced between versions
+  # Unless you have a need to, do not modify this value.
+  home.stateVersion = "24.11";
+
+  # Enable and configure ghostty
+  programs.ghostty.enable = true;
+  programs.ghostty.settings = {
+    # Launch ZSH on shell initialization calls
+    command = "zsh";
+
+    # Font Configuration
+    font-family = "MesloLGS NF"; # MesloLGS NerdFont Patched
+    font-size = 14.5; # A more reasonable size for a 1440p display
+  };
+
+  # Enable the Syncthing user-level service
+  services.syncthing.enable = true;
+}
